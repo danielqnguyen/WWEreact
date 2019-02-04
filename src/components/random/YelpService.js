dbd73b36bc94184d9c5bf6f80c102a15b621d3d0
ginkaikei <ginkaikei@gmail.com>
2019-02-04 14:28:51 -0800

    fixed api call

--- src/components/random/YelpService.js
+++ src/components/random/YelpService.js
@@ -7,9 +7,15 @@ class YelpApi {
     const config = {
       'Authorization': "Bearer 8svEkFic2Wd44Apmc7Cu8jWY1OmvjgVcsIjNteDVJ_phS7QuY8ruisHvYny_ZUywKwJ86cMlpBftmleMpn5dWXM-kVpapbkx-JWiDzyTZyr5ukJ8DIqMQXgCIw_-W3Yx",
     }
-    axios.get(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${food}&open_now=true&location=${location}&price=${dollars}`, { headers: config })
-      .then(onSuccess)
-      .catch(onError);
+    if (dollars === "") {
+      axios.get(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${food}&open_now=true&location=${location}`, { headers: config })
+        .then(onSuccess)
+        .catch(onError);
+    } else {
+      axios.get(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${food}&open_now=true&location=${location}&price=${dollars}`, { headers: config })
+        .then(onSuccess)
+        .catch(onError);
+    }
   }
 
   static yelpById(id, onSuccess, onError) {
