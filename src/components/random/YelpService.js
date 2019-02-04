1ed6d8af767a99d0247560c7839026e51927937b
Daniel Nguyen <44933972+danielqnguyen@users.noreply.github.com>
2019-02-04 13:26:02 -0800

    Update YelpService.js
    test

--- src/components/random/YelpService.js
+++ src/components/random/YelpService.js
@@ -5,7 +5,7 @@ dotenv.config();
 class YelpApi {
   static yelpTen(food, location, dollars, onSuccess, onError) {
     const config = {
-      'Authorization': ,
+      'Authorization': "Bearer 8svEkFic2Wd44Apmc7Cu8jWY1OmvjgVcsIjNteDVJ_phS7QuY8ruisHvYny_ZUywKwJ86cMlpBftmleMpn5dWXM-kVpapbkx-JWiDzyTZyr5ukJ8DIqMQXgCIw_-W3Yx",
     }
     axios.get(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${food}&open_now=true&location=${location}&price=${dollars}`, { headers: config })
       .then(onSuccess)
@@ -14,7 +14,7 @@ class YelpApi {
 
   static yelpById(id, onSuccess, onError) {
     const config = {
-      'Authorization': "",
+      'Authorization': "Bearer 8svEkFic2Wd44Apmc7Cu8jWY1OmvjgVcsIjNteDVJ_phS7QuY8ruisHvYny_ZUywKwJ86cMlpBftmleMpn5dWXM-kVpapbkx-JWiDzyTZyr5ukJ8DIqMQXgCIw_-W3Yx",
     }
     axios.get(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/${id}`, { headers: config })
       .then(onSuccess)
